3564dbcb2c959490e4d07aacc91eb05fb32b4665
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-02-04 15:10:34 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull pramod858/fraud-detection-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 pramod858/fraud-detection-app
