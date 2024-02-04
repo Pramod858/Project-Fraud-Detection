8066168d77b128f423a75c96bc19d2201960727d
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-02-04 18:48:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull pramod858/fraud-detection-app
+docker pull pramod858/fraud-detection-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 pramod858/fraud-detection-app
